0431dba174ba1c2fc85049d6995da6de3f7caac2
Clankcoll <Clankcoll@gmail.com>
2024-03-11 19:01:08 +0100

    added index handling and startet index page
    Added button to worktime to get back to home

--- internal/handlers/home.go
+++ internal/handlers/home.go
@@ -1,11 +1,29 @@
-// internal/handlers/union_calculator.go
 package handlers
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
+	"path/filepath"
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "HomeSweetHome.")
+	// Define the path to your index.html file
+	tmplPath, err := filepath.Abs("../../web/static/index.html")
+	if err != nil {
+		http.Error(w, "Failed to find the index.html file", http.StatusInternalServerError)
+		return
+	}
+
+	// Parse the index.html file
+	tmpl, err := template.ParseFiles(tmplPath)
+	if err != nil {
+		http.Error(w, "Failed to load index.html: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Execute the template without passing any data
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, "Failed to render index.html: "+err.Error(), http.StatusInternalServerError)
+	}
 }
